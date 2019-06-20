f06dc381205ccd842346383ff20cfde58a3236a3
Isaac Yong <isaac.yongty@gmail.com>
2019-06-21 06:51:01 +0800

    Refactor and bug fix
    -Code refactor
    -Missing bar line when there are only 3 or 6 chord progression issue fixed

--- progression-generator-app.js
+++ progression-generator-app.js
@@ -1,457 +1,25 @@
 // For debugging
       // Change spaceChar to 'K' for better visibility of space
       let spaceChar = '\xa0'
-          //spaceChar = 'K'
+           // spaceChar = 'K'
          
 // Initialization
 const key = {}
 let chords = {}
-getIsHalfBar = false
+isHalfBar = false
 
-// Functions
-const renderSpacing = function(spacing, chordLength) {
-   let resultSpacing = ''
-   
-   for(index = 0 ; index < spacing - chordLength ; index++) {
-       resultSpacing = resultSpacing + spaceChar 
-   }
-      return resultSpacing
-}
-
-const removeSuffix = function (getProgression) {
-
-   let newString = ''
-   let regex1 = ''
-   let regex2 = ''
-   let regex3 = ''
-
-   if (getProgression.includes('b')) {
-      regex1 = /b/g
-   }
-   if (getProgression.includes('m')) {
-      regex2 = /m/g
-   }
-   if (getProgression.includes('M')) {
-      regex3 = /M/g
-   }
-
-   newString = getProgression.replace(regex1,'') // remove Flat in progression if any
-   newString = newString.replace(regex2,'') // remove Minor in progression if any
-   newString = newString.replace(regex3,'') // remove Major in progression if any
-
-   return newString
-}
-
-const keyLibrary = function (getKey) {
-  
-   if (getKey === 'C') {
-      key.tonic = 'C'
-      key.supertonic = 'Dm7'
-      key.supertonicPar = 'D'
-      key.mediant = 'Em'
-      key.subdominant = 'F'
-      key.dominant = 'G'
-      key.submediant = 'Am'
-      key.leadingTone = 'Bm7b5'
-      key.subtonic = 'Bb'
-      key.supertonicPar = 'D'
-      key.mediantPar = 'E'
-      key.subdominantPar = 'Fm'
-      key.dominantPar = 'Gm'
-      key.submediantPar = 'A'
-      key.b6 = 'Ab'
-   }
-
-   if (getKey === 'Db') {
-      key.tonic = 'Db'
-      key.supertonic = 'Ebm7'
-      key.mediant = 'Fm'
-      key.subdominant = 'Gb'
-      key.dominant = 'Ab'
-      key.submediant = 'Bbm'
-      key.leadingTone = 'Cm7b5'
-      key.subtonic = 'B'
-      key.supertonicPar = 'Eb'
-      key.mediantPar = 'F'
-      key.subdominantPar = 'Gbm'
-      key.dominantPar = 'Abm'
-      key.submediantPar = 'Bb'
-      key.b6 = 'A'
-   }
-
-   if (getKey === 'D') {
-      key.tonic = 'D'
-      key.supertonic = 'Em7'
-      key.mediant = 'F#m'
-      key.subdominant = 'G'
-      key.dominant = 'A'
-      key.submediant = 'Bm'
-      key.leadingTone = 'C#m7b5'
-      key.subtonic = 'C'
-      key.supertonicPar ='E'
-      key.mediantPar = 'F#'
-      key.subdominantPar = 'Gm'
-      key.dominantPar = 'Am'
-      key.submediantPar = 'B'
-      key.b6 = 'Bb'
-   }
-
-   if (getKey === 'Eb') {
-      key.tonic = 'Eb'
-      key.supertonic = 'Fm7'
-      key.mediant = 'Gm'
-      key.subdominant = 'Ab'
-      key.dominant = 'Bb'
-      key.submediant = 'Cm'
-      key.leadingTone = 'Dm7b5'
-      key.subtonic = 'Db'
-      key.supertonicPar = 'F'
-      key.mediantPar = 'G'
-      key.subdominantPar = 'Abm'
-      key.dominantPar = 'Bbm'
-      key.submediantPar = 'C'
-      key.b6 = 'B'
-   }
-
-   if (getKey === 'E') {
-      key.tonic = 'E'
-      key.supertonic = 'F#m7'
-      key.mediant = 'G#m'
-      key.subdominant = 'A'
-      key.dominant = 'B'
-      key.submediant = 'C#m'
-      key.leadingTone = 'D#m7b5'
-      key.subtonic = 'D'
-      key.supertonicPar = 'F#'
-      key.mediantPar = 'G#'
-      key.subdominantPar = 'Am'
-      key.dominantPar = 'Bm'
-      key.submediantPar = 'C#'
-      key.b6 = 'C'
-   }
-
-   if (getKey === 'F') {
-      key.tonic = 'F'
-      key.supertonic = 'Gm7'
-      key.mediant = 'Am'
-      key.subdominant = 'Bb'
-      key.dominant = 'C'
-      key.submediant = 'Dm'
-      key.leadingTone = 'Em7b5'
-      key.subtonic = 'Eb'
-      key.supertonicPar = 'G'
-      key.mediantPar = 'A'
-      key.subdominantPar = 'Bbm'
-      key.dominantPar = 'Cm'
-      key.submediantPar = 'D'
-      key.b6 = 'Db'
-   }
-
-   if (getKey === 'Gb') {
-      key.tonic = 'Gb'
-      key.supertonic = 'Abm7'
-      key.mediant = 'Bbm'
-      key.subdominant = 'Cb'
-      key.dominant = 'Db'
-      key.submediant = 'Ebm'
-      key.leadingTone = 'Fm7b5'
-      key.subtonic = 'Fb'
-      key.supertonicPar = 'Ab'
-      key.mediantPar = 'Bb'
-      key.subdominantPar = 'Cbm'
-      key.dominantPar = 'Dbm'
-      key.submediantPar = 'Eb'
-      key.b6 = 'D'
-   }
-   if (getKey === 'G') {
-      key.tonic = 'G'
-      key.supertonic = 'Am7'
-      key.mediant = 'Bm'
-      key.subdominant = 'C'
-      key.dominant = 'D'
-      key.submediant = 'Em'
-      key.leadingTone = 'F#m7b5'
-      key.subtonic = 'F'
-      key.supertonicPar = 'A'
-      key.mediantPar = 'B'
-      key.subdominantPar = 'Cm'
-      key.dominantPar = 'Dm'
-      key.submediantPar = 'E'
-      key.b6 = 'Eb'
-   }
-
-   if (getKey === 'Ab') {
-      key.tonic = 'Ab'
-      key.supertonic = 'Bbm7'
-      key.mediant = 'Cm'
-      key.subdominant = 'Db'
-      key.dominant = 'Eb'
-      key.submediant = 'Fm'
-      key.leadingTone = 'Gm7b5'
-      key.subtonic = 'Gb'
-      key.supertonicPar = 'Bb'
-      key.mediantPar = 'C'
-      key.subdominantPar = 'Dbm'
-      key.dominantPar = 'Ebm'
-      key.submediantPar = 'F'
-      key.b6 = 'Fb'
-   }
-
-   if (getKey === 'A') {
-      key.tonic = 'A'
-      key.supertonic = 'Bm7'
-      key.mediant = 'C#m'
-      key.subdominant = 'D'
-      key.dominant = 'E'
-      key.submediant = 'F#m'
-      key.leadingTone = 'G#m7b5'
-      key.subtonic = 'G'
-      key.supertonicPar = 'B'
-      key.mediantPar = 'C#'
-      key.subdominantPar = 'Dm'
-      key.dominantPar = 'Em'
-      key.submediantPar = 'F#'
-      key.b6 = 'F'
-   }
-
-   if (getKey === 'Bb') {
-      key.tonic = 'Bb'
-      key.supertonic = 'Cm7'
-      key.mediant = 'Dm'
-      key.subdominant = 'Eb'
-      key.dominant = 'F'
-      key.submediant = 'Gm'
-      key.leadingTone = 'Am7b5'
-      key.subtonic = 'Ab'
-      key.supertonicPar = 'C'
-      key.mediantPar = 'D'
-      key.subdominantPar = 'Ebm'
-      key.dominantPar = 'Fm'
-      key.submediantPar = 'G'
-      key.b6 = 'Gb'
-   }
-
-   if (getKey === 'B') {
-      key.tonic = 'B'
-      key.supertonic = 'C#m7'
-      key.mediant = 'D#m'
-      key.subdominant = 'E'
-      key.dominant = 'F#'
-      key.submediant = 'G#m'
-      key.leadingTone = 'A#m7b5'
-      key.subtonic = 'A'
-      key.supertonicPar = 'C#'
-      key.mediantPar = 'D#'
-      key.subdominantPar = 'Em'
-      key.dominantPar = 'F#m'
-      key.submediantPar = 'G#'
-      key.b6 = 'G'
-   }
-
-   if (getKey === 'C#') {
-      key.tonic = 'C#'
-      key.supertonic = 'D#m7'
-      key.mediant = 'E#m'
-      key.subdominant = 'F#'
-      key.dominant = 'G#'
-      key.submediant = 'A#m'
-      key.leadingTone = 'B#m7b5'
-      key.subtonic = 'B'
-      key.supertonicPar = 'D#'
-      key.mediantPar = 'E#'
-      key.subdominantPar = 'F#m'
-      key.dominantPar = 'G#m'
-      key.submediantPar = 'A#'
-      key.b6 = 'A'
-   }
-
-   if (getKey === 'F#') {
-      key.tonic = 'F#'
-      key.supertonic = 'G#m7'
-      key.mediant = 'A#m'
-      key.subdominant = 'B'
-      key.dominant = 'C#'
-      key.submediant = 'D#m'
-      key.leadingTone = 'E#m7b5'
-      key.subtonic = 'E'
-      key.supertonicPar = 'G#'
-      key.mediantPar = 'A#'
-      key.subdominantPar = 'Bm'
-      key.dominantPar = 'C#m'
-      key.submediantPar = 'D#'
-      key.b6 = 'D'
-   }
-}
-
-const progressionGenerator = function (getSection, getKey, getProgression, getIsHalfBar,getSpace) {
-   
-   keyLibrary (getKey)
-
-   let progressionNumOnly = removeSuffix(getProgression)
-   console.log(progressionNumOnly)
-  
-   // Assign actual chords to progression
-   for (let index=0; index !==  getProgression.length; index++) {
-      
-      if  (getProgression[index] == '1') {
-      chords[index] =  key.tonic
-      
-      }  else if  (getProgression[index] == '2') {
-            if (getProgression[index + 1] === 'M') {
-              getProgression = getProgression.replace('M', '')
-              chords[index] =  key.supertonicPar
-            }  else {
-               chords[index] =  key.supertonic
-                }
-         
-         }
-         else if  (getProgression[index] == '3') {
-            if (getProgression[index + 1] === 'M') {
-               getProgression = getProgression.replace('M', '')
-               chords[index] =  key.mediantPar
-             }  else {
-               chords[index] =  key.mediant
-               }
-         
-         }
-         else if  (getProgression[index] == '4') {
-            if (getProgression[index + 1] === 'm') {
-               getProgression = getProgression.replace('m', '')
-               chords[index] =  key.subdominantPar
-            }  else {
-               chords[index] =  key.subdominant
-            }
-         }
-         else if  (getProgression[index] == '5') {
-            if (getProgression[index + 1] === 'm') {
-               getProgression = getProgression.replace('m', '')
-               chords[index] =  key.dominantPar
-            }  else {
-               chords[index] =  key.dominant
-               }
-            
-         
-         }
-         else if  (getProgression[index] == '6') {
-            if (getProgression[index + 1] === 'M') {
-               getProgression = getProgression.replace('M', '')
-               chords[index] =  key.submediantPar
-             } else {
-               chords[index] =  key.submediant
-               }
-         }
-         else if  (getProgression[index] == '7') {
-         chords[index] =  key.leadingTone
-         }
-         // Can handle Flat now
-         else if  (getProgression[index] === 'b' && getProgression[index + 1] === '7') {
-
-            getProgression = getProgression.replace('b', '')
-   
-            chords[index] =  key.subtonic
-         }
-         else if  (getProgression[index] === 'b' && getProgression[index + 1] === '6') {
-
-            getProgression = getProgression.replace('b', '')
-   
-            chords[index] =  key.b6
-         }
-        
-      }
-      console.log(chords)
-
-      
-   
-   if (getIsHalfBar === false) {
-   
-      let resultString = ''
-
-      if (getSection !== '') {
-         resultString = `${getSection}:\n`
-      } 
-   
-      for(let index = 0; index !== progressionNumOnly.length; index++) {
-      
-         resultString = resultString + `|\xa0`
-
-         if (chords[index].length === 1) {    
-              resultString = resultString + `[${chords[index]}]` + renderSpacing(getSpace, 0)               
-           }  else {
-            resultString = resultString + `[${chords[index]}]` + renderSpacing(getSpace, chords[index].length - 1) 
-         } 
-         if((index + 1) % 4 === 0 && index !== progressionNumOnly.length) {
-         resultString = resultString + `|\n`
-         }
-      }
-
-      if (progressionNumOnly !== undefined && progressionNumOnly.length % 4 !== 0) {
-            
-         resultString = resultString + `|\n\n`
-
-      }  else {
-         resultString = resultString + `\n`
-      }
-       
-      chords = {}
-      return resultString
-
-   }  else {
-         
-         halfSpace = Math.ceil((getSpace/2))
-         let resultString = ''
-
-         if (getSection !== '') {
-            resultString = `${getSection}:\n`
-         } 
-
-         for(let index = 0; index !== progressionNumOnly.length; index++) {
-         
-         // Extra 'if' code for half bar program
-            if ((index + 1) % 2 !== 0) {
-               resultString = resultString + '|\xa0'
-            }
-         // 
-
-            if (chords[index].length === 1) {    
-   
-                 resultString = resultString + `[${chords[index]}]` + renderSpacing(halfSpace, 0)    
-
-              }  else {
-                     resultString = resultString + `[${chords[index]}]` + renderSpacing(halfSpace, chords[index].length - 1) 
-              }
-            
-            if((index + 1) % 4 === 0 && index < progressionNumOnly.length) {
-               resultString = resultString + `|\n`
-            }
-         }
-         
-         if (progressionNumOnly !== undefined && progressionNumOnly.length % 2 !== 0) {
-            
-            resultString = resultString + `${renderSpacing(halfSpace + 3 ,0)}|\n\n`
-            
-            // Note: in [Xyyy] halfBarSpace,  included __yyy_KK, y + K = halfBarSpace, empty space = constant = 3
-         }
-
-         resultString = resultString + `\n`
-           
-         chords = {}
-         return resultString
-      }
-   
-}
-
-      
-// HTML
+let standardFullBarSpace = 18
+let standardHalfBarSpace = 14
 
 document.querySelector('#prog-choice-1').addEventListener('click', function(e) {
-   getIsHalfBar = false
+   isHalfBar = false
+   document.querySelector('#spacing').value = standardFullBarSpace
 })
 
 document.querySelector('#prog-choice-2').addEventListener('click', function(e) {
-   getIsHalfBar = true
+   isHalfBar = true
+   document.querySelector('#spacing').value = standardHalfBarSpace
 })
-
-
 
 document.querySelector('#prog-common-progression').addEventListener('change', function (e) {
    document.getElementById('prog-text').value = e.target.value
@@ -466,37 +34,38 @@ document.querySelector('#prog-form').addEventListener('submit', function (e) {
       // console.log(e.target.elements.inputKey.value)
       // console.log(e.target.elements.inputProgression.value)
 
-   let result = document.createElement('p')
+   let result = ''
 
-   result.textContent = progressionGenerator(e.target.elements.inputSection.value,
-                                             e.target.elements.inputKey.value,
-                                             e.target.elements.inputProgression.value,
-                                             getIsHalfBar,
-                                             e.target.elements.inputSpacing.value)
+   keyLibrary (e.target.elements.inputKey.value)
+   progressionNumOnly = removeSuffix(e.target.elements.inputProgression.value)
+   chords = assignChordsToProg(progressionNumOnly)
 
-   document.getElementById('outputResult').value = document.getElementById('outputResult').value + result.textContent
-   console.log(document.getElementById('outputResult').value)
+   if (isHalfBar === false) {
+      result = fullBarProg(e.target.elements.inputSection.value, 
+                                       progressionNumOnly, 
+                                       e.target.elements.inputSpacing.value,
+                                       chords
+   )}  else {
+      result = halfBarProg(e.target.elements.inputSection.value, 
+                                       progressionNumOnly, 
+                                       e.target.elements.inputSpacing.value,
+                                       chords
+   )}
    
+   document.getElementById('outputResult').value += result
    document.getElementById('prog-section').value = ''
 })
 
 
 document.querySelector('#clear').addEventListener('click', function (e) {
    document.getElementById('outputResult').value = ''
-   
 })
 
 document.querySelector('#copy').addEventListener('click', function (e) {
    document.execCommand('copy') = document.getElementById('outputResult').value 
-   
 })
 
-document.addEventListener('keydown', function (e) {
-   console.log(e)
-   if (e.code === 'v') {
-      console.log('lol')
-   }
-})
+
 
 
 
